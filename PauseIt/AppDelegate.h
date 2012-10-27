eeae468d5b9e950d9369a30cc6c72ed8a9c06e10
Siyuan Zhang <onepill@gmail.com>
2012-10-28 00:24:47 +0800

    listen to media keys

--- PauseIt/AppDelegate.h
+++ PauseIt/AppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSApplication <NSObject>
 
 @property (assign) IBOutlet NSWindow *window;
 
--- PauseIt/AppDelegate.m
+++ PauseIt/AppDelegate.m
@@ -7,12 +7,34 @@
 //
 
 #import "AppDelegate.h"
+#import <IOKit/hidsystem/ev_keymap.h>
+#import "AppleMediaKeyController.h"
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    [AppleMediaKeyController sharedController];
+    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+    [center addObserver:self selector:@selector(playPauseKeyNotification) name:MediaKeyPlayPauseNotification object:nil];
+    [center addObserver:self selector:@selector(nextKeyNotification) name:MediaKeyNextNotification object:nil];
+    [center addObserver:self selector:@selector(previousKeyNotification) name:MediaKeyPreviousNotification object:nil];
+}
+
+- (void)playPauseKeyNotification {
+    NSLog(@"play pause key pressed");
+}
+
+- (void)nextKeyNotification {
+    NSLog(@"next key pressed");
+}
+
+- (void)previousKeyNotification {
+    NSLog(@"previous key pressed");
+    NSString *myScript = @"tell application \"iTunes\" \n activate \n end tell";
+    NSAppleScript *script = [[NSAppleScript alloc] initWithSource:myScript];
+    [script executeAndReturnError:nil];
 }
 
 @end
